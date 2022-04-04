2ad8cdad31ca9befb147bfeb54c391927f5850d0
ArensTV <janarens@yahoo.de>
2022-04-04 14:41:40 +0200

    Arrange,Act,Assert

--- WuerfelspielTests/WuerfelTests.cs
+++ WuerfelspielTests/WuerfelTests.cs
@@ -10,34 +10,49 @@ namespace WuerfelspielTests
         [TestMethod]
         public void WuerfelErstellen()
         {
+            // Arrange
             Wuerfel w1 = new Wuerfel();
-            Assert.IsNotNull(w1);
+            // Act
+
+            //Assert
+            Assert.AreEqual(w1.anzahlSeiten, 6);
         }
 
         [TestMethod]
         public void D6Erstellen()
         {
+            // Arrange
             int anzahlSeiten = 6;
             Wuerfel w2 = new Wuerfel(anzahlSeiten);
+            // Act
+
+            // Assert
             Assert.AreEqual(w2.AnzahlSeiten, anzahlSeiten);
         }
 
         [TestMethod]
         public void WuerfelWuerfeln()
         {
+            // Arrange
             Wuerfel w3 = new Wuerfel();
+
+            // Act
             int result = w3.Wuerfeln();
 
+            // Assert
             Assert.IsNotNull(result);
         }
 
         [TestMethod]
         public void WuerfelLetztesErgebnisAbrufen()
         {
+            // Arrange
             Wuerfel w4 = new Wuerfel();
+            // Act
             int result = w4.Wuerfeln();
             int result_save = w4.LetztesErgebnis;
 
+            // Assert
             Assert.AreEqual(result, result_save);
         }
     }
